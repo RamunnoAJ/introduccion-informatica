291f3c64a7ace7fdd05c37341e2b08c4aca1fec2
RamunnoAJ <ramunnoagustin@gmail.com>
2024-04-26 18:27:36 -0300

    Elimino variables innecesarias

--- tp-ciclos/tp-ciclos.pas
+++ tp-ciclos/tp-ciclos.pas
@@ -5,7 +5,6 @@ Const
     PrecioMargaritas = 500;
 Var
     N, i,  Cant, CantR, CantF, CantM: byte;
-    MontoRosas, MontoFresias, MontoMargaritas: real;
     Arch: text;
     TFlor: char;
 Begin
@@ -13,7 +12,6 @@ Assign(Arch, 'Floreria.txt');
 Reset(Arch);
 Readln(Arch, N);
 
-MontoRosas := 0; MontoFresias := 0; MontoMargaritas := 0;
 CantR := 0; CantF := 0; CantM := 0;
 
 For i := 1 To N Do
@@ -26,13 +24,9 @@ Begin
     end;
 end;
 
-MontoRosas := MontoRosas + (CantR * PrecioRosas);
-MontoFresias := MontoFresias + (CantF * PrecioFresias);
-MontoMargaritas := MontoMargaritas + (CantM * PrecioMargaritas);
-
-Writeln('Por la venta de rosas se recaudaron: $', MontoRosas:8:2);
-Writeln('Por la venta de fresias se recaudaron: $', MontoFresias:8:2);
-Writeln('Por la venta de margaritas se recaudaron: $', MontoMargaritas:8:2);
+Writeln('Por la venta de rosas se recaudaron: $', (CantR * PrecioRosas));
+Writeln('Por la venta de fresias se recaudaron: $', (CantF * PrecioFresias));
+Writeln('Por la venta de margaritas se recaudaron: $', (CantM * PrecioMargaritas));
 
 if (CantR > CantF) and (CantR > CantM) then
     Writeln('La flor más vendida fue la rosa, y se vendieron ', CantR)
